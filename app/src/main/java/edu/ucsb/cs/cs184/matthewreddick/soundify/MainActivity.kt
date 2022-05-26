5d18f50c169bbd6e141a9d7851ee2f03d2bd68de
Erwan Fraisse <efraisse@ucsb.edu>
2022-05-25 20:44:44 -0700

    Added an xml format for spotify and soundcloud songs. I have also finished the UI layout for the home fragment.

--- app/src/main/java/edu/ucsb/cs/cs184/matthewreddick/soundify/MainActivity.kt
+++ app/src/main/java/edu/ucsb/cs/cs184/matthewreddick/soundify/MainActivity.kt
@@ -3,6 +3,7 @@ package edu.ucsb.cs.cs184.matthewreddick.soundify
 import android.os.Bundle
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
