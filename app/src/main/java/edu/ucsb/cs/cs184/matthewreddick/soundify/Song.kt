d993d729037043f7ee2816d1ec15334e548e2f91
Alon Katz <alonkatz@ucsb.edu>
2022-05-29 11:47:47 -0700

    Created a list of songs using the firebase data.

--- app/src/main/java/edu/ucsb/cs/cs184/matthewreddick/soundify/Song.kt
+++ app/src/main/java/edu/ucsb/cs/cs184/matthewreddick/soundify/Song.kt
@@ -1,16 +1,39 @@
 package edu.ucsb.cs.cs184.matthewreddick.soundify
 
 import android.media.Image
+import android.util.Log
+import kotlin.math.log
 
 
-class Song {
-    private var title :String? = null
-    private var artist: String? = null
-    private var album: String? = null
-    private var picture: Image? = null
-//    private var audioFile: Not sure what is right way to implement this yet
-    private var duration: Int? = null
-    private var id: Int? = null
+class Song(
+    title: String,
+    artist: String,
+    album: String,
+    imageUrl: String,
+    audioUrl: String,
+    duration: Int,
+    id: Int
+) {
+    private var title :String? = title
+    private var artist: String? = artist
+    private var album: String? = album
+    private var imageUrl: String? = imageUrl
+    private var audioUrl: String? = audioUrl
+    private var duration: Int? = duration
+    private var id: Int? = id
+
+    override fun toString(): String {
+        var output = ""
+        output += "Title: $title\n"
+        output += "Artist: $artist\n"
+        output += "Album: $album\n"
+        output += "ImageUrl: $imageUrl\n"
+        output += "AudioUrl: $audioUrl\n"
+        output += "Duration: $duration\n"
+        output += "ID: $id\n"
+        return output
+
+    }
 
 
 }
