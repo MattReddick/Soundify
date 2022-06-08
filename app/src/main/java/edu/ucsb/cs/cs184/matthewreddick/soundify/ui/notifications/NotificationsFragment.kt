b10bc1e968b323a4ce569775bb720cb8fae76875
Erwan Fraisse <efraisse@ucsb.edu>
2022-06-08 15:40:02 -0700

    updated the ui in the mediaplayer fragment so that the buttons are more even instead of the top three being significantly bigger than the shuffle and loop buttons, also fixed a small bug where the play button was not updating when playing a song from the queue

--- app/src/main/java/edu/ucsb/cs/cs184/matthewreddick/soundify/ui/notifications/NotificationsFragment.kt
+++ app/src/main/java/edu/ucsb/cs/cs184/matthewreddick/soundify/ui/notifications/NotificationsFragment.kt
@@ -14,6 +14,7 @@ import androidx.fragment.app.Fragment
 import edu.ucsb.cs.cs184.matthewreddick.soundify.R
 import edu.ucsb.cs.cs184.matthewreddick.soundify.databinding.FragmentNotificationsBinding
 import edu.ucsb.cs.cs184.matthewreddick.soundify.playerObject
+import edu.ucsb.cs.cs184.matthewreddick.soundify.ui.dashboard.playPause
 
 lateinit var queueList: NotificationsFragment.CustomAdapterQueue
 private var con : Context?= null
@@ -97,6 +98,7 @@ class NotificationsFragment : Fragment() {
                     val tempIndex = playerObject.queue!!.indexOf(playerObject.queue!![p0])
                     playerObject.currentSongIndex = tempIndex - 1
                     playerObject.playNext()
+                    playPause = true
                     Toast.makeText(con, "now playing " + playerObject.queue!![p0].getTitle(), Toast.LENGTH_SHORT).show()
                 }
             }
@@ -106,6 +108,7 @@ class NotificationsFragment : Fragment() {
                     val tempIndex = playerObject.queue!!.indexOf(playerObject.queue!![p0])
                     playerObject.currentSongIndex = tempIndex - 1
                     playerObject.playNext()
+                    playPause = true
                     Toast.makeText(con, "now playing " + playerObject.queue!![p0].getTitle(), Toast.LENGTH_SHORT).show()
                 }
             }
